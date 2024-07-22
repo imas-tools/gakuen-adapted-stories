79963d1b51c42392fc4f207b2bfd49ce44e32c93
Badmingrams <161575566+Badmingrams@users.noreply.github.com>
2024-07-22 22:37:46 +0800

    fix: quote in csv

--- main.py
+++ main.py
@@ -1,28 +1,34 @@
 import os
+from imas_tools.story.story_csv import StoryCsv
 from imas_tools.story.gakuen_parser import parse_messages
 
 
 def gakuen_txt_to_sc_csv(gakuen_txt: str, txt_name_without_ext: str) -> str:
     parsed = parse_messages(gakuen_txt)
-    sc_csv = "id,name,text,trans\n"
+    sc_csv = StoryCsv.new_empty_csv(f"{txt_name_without_ext}.txt")
     for line in parsed:
         if line["__tag__"] == "message" or line["__tag__"] == "narration":
             if line.get("text"):
-                sc_csv += f"0000000000000,{line.get('name','__narration__')},{line['text']},\n"
+                sc_csv.append_line(
+                    {
+                        "id": "0000000000000",
+                        "name": line.get("name", "__narration__"),
+                        "text": line["text"],
+                        "trans": "",
+                    }
+                )
         if line["__tag__"] == "title":
             if line.get("title"):
-                sc_csv += f"0000000000000,__title__,{line['title']},\n"
+                sc_csv.append_line({"id": "0000000000000", "name": "__title__", "text": line["title"], "trans": ""})
         if line["__tag__"] == "choicegroup":
             if isinstance(line["choices"], list):
                 for choice in line["choices"]:
-                    sc_csv += f"select,,{choice['text']},\n"
+                    sc_csv.append_line({"id": "select", "name": "", "text": choice["text"], "trans": ""})
             elif isinstance(line["choices"], dict):
-                sc_csv += f"select,,{line['choices']['text']},\n"
+                sc_csv.append_line({"id": "select", "name": "", "text": line["choices"]["text"], "trans": ""})
             else:
                 raise ValueError(f"Unknown choice type: {line['choices']}")
-    sc_csv += f"info,{txt_name_without_ext}.txt,,\n"
-    sc_csv += f"译者,,,\n"
-    return sc_csv
+    return str(sc_csv)
 
 
 if __name__ == "__main__":
